96d6e530b775a22fc0ae4bd177c4e6d731422f9a
ninguemestaprotegido <pierresilva267@gmail.com>
2025-02-16 20:30:00 -0300

    Updated database connection

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from app import create_app
+import os
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Pega a porta definida pelo Railway ou usa a 8080 como padrão
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port, debug=True)
