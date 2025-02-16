a43b583c7eb301fcd03f1aad84c96c105ad5a881
ninguemestaprotegido <62519950+ninguemestaprotegido@users.noreply.github.com>
2025-02-16 15:58:07 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -12,10 +12,14 @@ def create_app():
 
     # Obtendo a URL do banco de dados do ambiente
     database_url = os.getenv('DATABASE_URL')
-    if database_url and database_url.startswith("postgres://"):
+
+    if not database_url:
+        raise ValueError("DATABASE_URL não foi encontrada. Verifique as variáveis de ambiente.")
+
+    if database_url.startswith("postgres://"):
         database_url = database_url.replace("postgres://", "postgresql://", 1)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = database_url or 'sqlite:///app.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_url
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
